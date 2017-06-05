ff27a793e5c9b77d77d0c63a52af77bc63c3902e
HOME <HOME@192.168.0.12>
2017-06-05 17:44:10 +0530

    WebApp Calculator-BasicCalc.java code-Anusha.Ch

--- src/calculator/BasicCalc.java
+++ src/calculator/BasicCalc.java
@@ -8,5 +8,36 @@ public class BasicCalc
 	//include getters and setters method to access variables
 	
 	//include methods for operations
-	
+double a,b,c;
+public double geta() {
+return a;
+}
+public void seta(double a) {
+	this.a=a;
+}
+public double getb() {
+return b;
+}
+public void setb(double b) {
+	this.b=b;
+}
+double add() {
+c=a+b;
+	return(c);
+}
+double substract() {
+c=a-b;
+	return(c);
+
+}
+double multiply() {
+c=a*b;
+	return(c);
+}
+double divide() {
+c=a/b;
+	return(c);
+}
+
+
 }
